b8c328b3d08a3ad1431fae024a40706941da0c75
Pete Marshall <petemarshall77@gmail.com>
2020-12-17 17:29:13 -0800

    Initial MVP of logger program

---
+++ weather-logger.py
@@ -0,0 +1,29 @@
+#
+# Simple weather data logger
+#
+
+import schedule
+import time
+from serial import Serial
+
+portname = '/dev/ttyUSB0'
+portspeed = 9600
+logfilename = 'weather-log.txt'
+
+def log_data():
+    log_time = time.asctime()
+    serial = Serial(portname, portspeed, timeout=None)
+    time.sleep(3)  # wait for Arduino to reset
+    serial.write(b'F')
+    response = serial.readline().decode('utf-8')
+    print(log_time, response.rstrip())
+    with open(logfilename, 'a') as f:
+        f.write(log_time + ',' + response)
+    
+    
+log_data() # do once on startup
+schedule.every(15).seconds.do(log_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
