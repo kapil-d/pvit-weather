25666690806760e57c8913d6862dc3c917b1a32e
Pete Marshall <petemarshall77@gmail.com>
2021-01-16 13:34:36 -0800

    Add station id, updates from 1-9-21 meeting

--- weather-logger.py
+++ weather-logger.py
@@ -6,9 +6,10 @@ import schedule
 import time
 from serial import Serial
 
-portname = '/dev/ttyUSB0'
+portname = '/dev/ttyUSB0'  # portname = 'COM1' on Windows
 portspeed = 9600
 logfilename = 'weather-log.txt'
+station_name = 'ABC'       # should be unique
 
 def log_data():
     log_time = time.asctime()
@@ -16,9 +17,9 @@ def log_data():
     time.sleep(3)  # wait for Arduino to reset
     serial.write(b'F')
     response = serial.readline().decode('utf-8')
-    print(log_time, response.rstrip())
+    print(station_id, log_time, response.rstrip())
     with open(logfilename, 'a') as f:
-        f.write(log_time + ',' + response)
+        f.write(station_id * log_time + ',' + response)
     
     
 log_data() # do once on startup
