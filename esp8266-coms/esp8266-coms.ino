fa24be6f3da5a51a2600ce4266ce876af4d322a4
Pete Marshall <petemarshall77@gmail.com>
2021-04-13 19:50:04 -0700

    Added esp8266 serial communications

---
+++ esp8266-coms/esp8266-coms.ino
@@ -0,0 +1,37 @@
+//
+// COMUNICATE WITH ESP8266 DEVICE - Simple Serial Passthru
+//
+// Use serial monitor to communicate with Arduino; Arduino passes Tx/Rx to ESP8266
+//
+// Pins: 2 - Arduino Rx to ESP8266 Tx
+//       3 - Arduino Tx to ESP8266 Rx via voltage divider
+//       Arduino Gnd to ESP8266 Gnd
+//       Arduino 3.3V to ESP8266 Vcc
+//       Pull CH_PD on ESP8266 HIGH to 3.3v with Pullup Resistor
+//
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial ESP8266Serial(2,3);   // 2:Rx, 3:Tx
+
+void setup()
+{
+  Serial.begin(9600);
+  ESP8266Serial.begin(115200);
+
+  Serial.println("Set NL and CR in Serial Monitor.");
+  Serial.println("Ready.");
+  Serial.println("");
+
+}
+
+
+void loop()
+{
+  if (ESP8266Serial.available()) {
+    Serial.write(ESP8266Serial.read());
+  }
+  if (Serial.available()) {
+    ESP8266Serial.write(Serial.read());
+  }
+}
